ab75f95c597c2c343a22d26600a413ab39f6f469
Ryan <thisianemailaddressthatis@gmail.com>
2022-08-22 18:32:43 -0500

    finished logic and html generation

---
+++ index.js
@@ -0,0 +1,211 @@
+const inquirer = require("inquirer");
+const fs = require("fs");
+const { inherits } = require("util");
+const Manager = require("./lib/Manager");
+const Engineer = require("./lib/Engineer");
+const Intern = require("./lib/Intern");
+const employees = [];
+
+const managerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "what is the manager's name",
+  },
+  {
+    type: "number",
+    name: "id",
+    message: "what is the manager's employee id",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "what is the manager's email address?",
+  },
+  {
+    type: "input",
+    name: "office",
+    message: "what is the manager's office number?",
+  },
+];
+
+const engineerQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "what is the engineer's name",
+  },
+  {
+    type: "number",
+    name: "id",
+    message: "what is the engineer's employee id",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "what is the engineer's email address?",
+  },
+  {
+    type: "input",
+    name: "github",
+    message: "what is the engineer's github?",
+  },
+];
+
+const internQuestions = [
+  {
+    type: "input",
+    name: "name",
+    message: "what is the intern's name",
+  },
+  {
+    type: "number",
+    name: "id",
+    message: "what is the intern's employee id",
+  },
+  {
+    type: "input",
+    name: "email",
+    message: "what is the intern's email address?",
+  },
+  {
+    type: "input",
+    name: "school",
+    message: "whhere did the intern go to school?",
+  },
+];
+
+const roleQuestion = [
+  {
+    type: "list",
+    name: "role",
+    message:
+      "What employee type do you want to add? Or select finish to build team.",
+    choices: ["Engineer", "Intern", "Finish"],
+  },
+];
+
+function makeCard(employeeInfo) {
+  const role = employeeInfo.getRole();
+  if (role === "Manager") {
+    return ` <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${employeeInfo.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+      <p class="card-text">ID:${employeeInfo.id}</p>
+      <p class="card-text">Email:<a href=mailto:${employeeInfo.email}>${employeeInfo.email}</a></p>
+      <p class="card-text">Office Number:${employeeInfo.office}</p>
+    </div>
+  </div>`;
+  }
+  if (role === "Engineer") {
+    return ` <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${employeeInfo.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+      <p class="card-text">ID:${employeeInfo.id}</p>
+      <p class="card-text">Email:<a href=mailto:${employeeInfo.email} target="_blank">${employeeInfo.email}</a></p>
+      <p class="card-text">Github:<a href=https://github.com/${employeeInfo.github}>${employeeInfo.github}</p>
+    </div>
+  </div>`;
+  }
+  if (role === "Intern") {
+    return ` <div class="card" style="width: 18rem;">
+    <div class="card-body">
+      <h5 class="card-title">${employeeInfo.name}</h5>
+      <h6 class="card-subtitle mb-2 text-muted">${role}</h6>
+      <p class="card-text">ID:${employeeInfo.id}</p>
+      <p class="card-text">Email:<a href=mailto:${employeeInfo.email}>${employeeInfo.email}</a></p>
+      <p class="card-text">School:${employeeInfo.school}</p>
+    </div>
+  </div>`;
+  }
+}
+
+function generateHtml() {
+  let html = "";
+  for (let i = 0; i < employees.length; i++) {
+    html += makeCard(employees[i]);
+  }
+  return `<!DOCTYPE html>
+    <html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <title>Team Profile</title>
+        <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/css/bootstrap.min.css" integrity="sha384-xOolHFLEh07PJGoPkLv1IbcEPTNtaed2xpHsD9ESMhqIYd0nLMwNLD69Npy4HI+N" crossorigin="anonymous">
+    </head>
+    <body>
+    <div class="jumbotron jumbotron-fluid">
+  <div class="container">
+    <h1 class="display-4">My Team</h1>
+  </div>
+</div>
+${html}
+
+    
+    <script src="https://cdn.jsdelivr.net/npm/jquery@3.5.1/dist/jquery.slim.min.js" integrity="sha384-DfXdz2htPH0lsSSs5nCTpuj/zy4C+OGpamoFVy38MVBnE+IbbVYUew+OrCXaRkfj" crossorigin="anonymous"></script>
+<script src="https://cdn.jsdelivr.net/npm/bootstrap@4.6.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-Fy6S3B9q64WdZWQUiU+q4/2Lc9npb8tCaSX9FK7E8HnRr0Jz8D6OP9dO5Vg3Q9ct" crossorigin="anonymous"></script>
+    </body>
+    </html>
+  
+      
+  
+  
+  `;
+}
+
+function writeFile(html) {
+  console.log(employees);
+  const path = "./sample/index.html";
+
+  fs.writeFile(path, html, function (err) {
+    if (err) throw err;
+    console.log("Index.html generated");
+  });
+}
+
+function createEmployee() {
+  inquirer.prompt(roleQuestion).then((response) => {
+    if (response.role === "Intern") {
+      inquirer.prompt(internQuestions).then((resp) => {
+        const intern = new Intern(resp.name, resp.id, resp.email, resp.school);
+        employees.push(intern);
+        createEmployee();
+      });
+    }
+    if (response.role === "Engineer") {
+      inquirer.prompt(engineerQuestions).then((resp) => {
+        const engineer = new Engineer(
+          resp.name,
+          resp.id,
+          resp.email,
+          resp.github
+        );
+        employees.push(engineer);
+        createEmployee();
+      });
+    }
+    if (response.role === "Finish") {
+      const html = generateHtml();
+      writeFile(html);
+    }
+  });
+}
+
+function init() {
+  inquirer.prompt(managerQuestions).then((response) => {
+    const manager = new Manager(
+      response.name,
+      response.id,
+      response.email,
+      response.office
+    );
+    employees.push(manager);
+    createEmployee();
+  });
+}
+
+// Function call to initialize app
+init();
--- tests/Employee.test.js
+++ tests/Employee.test.js
@@ -1,1 +1,8 @@
 const employeeTest = require("../lib/Employee");
+
+describe("getName", () => {
+  it("should return the name of the employee", () => {
+    expect(fizzBuzz([1])).toBe("1");
+    expect(fizzBuzz([1, 2])).toBe("1, 2");
+  });
+});
